bc3204c70cf28bb856c82538552f2af7ef64a2d5
Josue Cruz <josue.cruz02@utrgv.edu>
2022-11-21 13:31:07 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int32_value, bool_value, double_value) VALUES (321312, 0, 213213.21321);
+
+--UPDATE sandbox SET string_value = 'CS 1101 is my favorite class' WHERE id = 14;
+
+--DELETE FROM sandbox WHERE id = 48;
